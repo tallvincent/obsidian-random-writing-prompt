bcfa108ad6c540ca7e981705b58862b6ecb8e06a
Vincent Talladira <vincent@tallvincent.com>
2026-07-06 16:32:31 +0930

    Initial commit.

---
+++ src/settings.ts
@@ -0,0 +1,38 @@
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import RandomWritingPrompt from './main';
+
+export interface Settings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: Settings = {
+	mySetting: 'default',
+};
+
+export class SettingTab extends PluginSettingTab {
+	plugin: RandomWritingPrompt;
+
+	constructor(app: App, plugin: RandomWritingPrompt) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Settings #1')
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+	}
+}
