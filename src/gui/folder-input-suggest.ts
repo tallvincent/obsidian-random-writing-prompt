15e6cdcebe3e5eb98b5c8ad34fddac702d1ce764
Vincent Talladira <vincent@tallvincent.com>
2026-07-07 17:51:12 +0930

    Setting for prompts folder and updated other functions to use the folder.

---
+++ src/gui/folder-input-suggest.ts
@@ -0,0 +1,33 @@
+import { AbstractInputSuggest, App, TFolder } from 'obsidian';
+
+export class FolderInputSuggest extends AbstractInputSuggest<TFolder> {
+	private onChooseFn: (folder: TFolder) => void;
+
+	constructor(
+		app: App,
+		inputEl: HTMLInputElement,
+		onChoose: (folder: TFolder) => void,
+	) {
+		super(app, inputEl);
+		this.onChooseFn = onChoose;
+	}
+
+	getSuggestions(query: string): TFolder[] {
+		const folders = this.app.vault
+			.getAllLoadedFiles()
+			.filter((f): f is TFolder => f instanceof TFolder);
+		const lower = query.toLowerCase();
+		return folders.filter((folder) =>
+			folder.path.toLowerCase().includes(lower),
+		);
+	}
+
+	renderSuggestion(folder: TFolder, el: HTMLElement): void {
+		el.createDiv({ text: folder.path || '/' });
+	}
+
+	selectSuggestion(folder: TFolder, _evt: MouseEvent | KeyboardEvent): void {
+		this.setValue(folder.path);
+		this.onChooseFn(folder);
+	}
+}
