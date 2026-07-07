764b302b528019049d741f76f5db677d484d2c6a
Vincent Talladira <vincent@tallvincent.com>
2026-07-07 16:22:04 +0930

    Bump min app version. Basic functionality for 4 core commands.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
+	App,
 	Modal,
 	Notice,
 	Plugin,
+	Setting,
+	SuggestModal,
+	TFile,
 } from 'obsidian';
 import {
 	DEFAULT_SETTINGS,
 	Settings,
 	SettingTab,
 } from './settings';
-
-// Remember to rename these classes and interfaces!
 
 export default class RandomWritingPrompt extends Plugin {
 	settings!: Settings;
@@ -21,67 +20,120 @@ export default class RandomWritingPrompt extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
+		// TODO: not sure this will be needed but maybe to show all the prompts that have been filled / not filled
 		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new CustomModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new CustomModal(this.app).open();
-					}
+			id: 'open-random-prompt',
+			name: 'Open random prompt',
+			callback: async () => {
+				// Find the prompts file
+				// TODO: can get this from settings
+				const fileName: string = 'Random Writing Prompts.md';
+				const file = this.app.vault.getAbstractFileByPath(fileName);
+				if (!(file instanceof TFile)) return [];
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// Find all prompts (lines) without links
+				const possiblePrompts: Prompt[] = await this.getPromptsFromFile(file);
+
+				if (possiblePrompts.length === 0) {
+					return new Notice('Your prompts file is currently empty');
 				}
-				return false;
+
+				// Select random prompt
+				const prompt: Prompt | undefined = this.getRandomElement(possiblePrompts.filter((p) => !p.started));
+
+				if (!prompt) {
+					return new Notice('Your prompts file is currently empty');
+				}
+
+				// Create a new file with that prompt as name
+				// TODO: add in some metadata - could also allow user to specify template
+				const newFile: TFile = await this.app.vault.create(prompt.title + '.md', '');
+
+				// Create backlink in prompt file
+				await this.app.vault.process(file, (data) => {
+					return data.replace(prompt.title, `[[${prompt.title}]]`);
+				})
+
+				// Open newly created prompt
+				return await this.app.workspace.getLeaf('tab').openFile(newFile);
 			},
 		});
+
+		this.addCommand({
+			id: 'open-main-prompts-file',
+			name: 'Open main prompts file',
+			callback: async () => {
+				const fileName: string = 'Random Writing Prompts.md';
+				const file = await this.getMainPromptsFile(fileName);
+				if (file) {
+					return await this.app.workspace.getLeaf('tab').openFile(file);
+				} else {
+					new Notice('You do not have a main prompts file');
+				}
+			}
+		})
+
+		this.addCommand({
+			id: 'show-all-prompts',
+			name: 'Show all prompts',
+			callback: async () => {
+				const file = await this.getMainPromptsFile('Random Writing Prompts.md');
+				if (!(file instanceof TFile)) return [];
+				const prompts: Prompt[] = await this.getPromptsFromFile(file);
+				return new AllPromptsModal(this.app, prompts, file).open();
+			}
+		})
+
+		this.addCommand({
+			id: 'add-prompt',
+			name: 'Add new prompt to file',
+			callback: async () => {
+				const file = await this.getMainPromptsFile('Random Writing Prompts.md');
+				if (!(file instanceof TFile)) return [];
+				return new AddPromptModal(this.app, file).open();
+			}
+		})
+
+		this.addCommand({
+			id: 'open-random-started-prompt',
+			name: 'Open random started prompt',
+			callback: async () => {
+				// Find the prompts file
+				// TODO: can get this from settings
+				const fileName: string = 'Random Writing Prompts.md';
+				const file = this.app.vault.getAbstractFileByPath(fileName);
+				if (!(file instanceof TFile)) return [];
+
+				// Find all prompts (lines) without links
+				const possiblePrompts: Prompt[] = await this.getPromptsFromFile(file);
+
+				if (!possiblePrompts) {
+					return new Notice('Your prompts file is currently empty');
+				}
+
+				// Select random prompt
+				const prompt: Prompt | undefined = this.getRandomElement(possiblePrompts.filter((p) => p.started));
+
+				if (!prompt) {
+					return new Notice('Your prompts file is currently empty');
+				}
+
+				const promptFile = this.app.vault.getAbstractFileByPath(prompt.title + '.md');
+				if (!(promptFile instanceof TFile)) return [];
+
+				// Open newly created prompt
+				return await this.app.workspace.getLeaf('tab').openFile(promptFile);
+			}
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-
 	}
 
 	onunload() { }
@@ -97,16 +149,119 @@ export default class RandomWritingPrompt extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class CustomModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
+	isBacklinkLine(line: string) {
+		// Only matches regular links: [[target]] or [[target|alias]] (NOT embeds ![[...]])
+		return /\[\[[^\]]+\]\]/.test(line) && !/!\[\[[^\]]+\]\]/.test(line);
 	}
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
+	getRandomElement<T>(arr: T[]): T | undefined {
+		if (arr.length === 0) return undefined;
+
+		const index = Math.floor(Math.random() * arr.length);
+		return arr[index];
+	}
+
+	async getMainPromptsFile(fileName: string) {
+		const file = this.app.vault.getAbstractFileByPath(fileName);
+		if (!(file instanceof TFile)) return null;
+		return file;
+	}
+
+	async getPromptsFromFile(file: TFile) {
+		const content: string = await this.app.vault.read(file);
+		if (!content) {
+			return [];
+		}
+		const lines: string[] = content.split(/\r?\n/);
+		return lines.filter((l) => !!l).map((l) => {
+			return {
+				title: l.replaceAll('[', '').replaceAll(']', ''),
+				started: this.isBacklinkLine(l)
+			}
+		})
+	}
+}
+
+interface Prompt {
+	title: string,
+	started: boolean
+}
+
+class AllPromptsModal extends SuggestModal<Prompt> {
+	prompts: Prompt[];
+	promptsFile: TFile;
+
+	constructor(
+		app: App,
+		prompts: Prompt[],
+		promptsFile: TFile,
+	) {
+		super(app);
+		this.prompts = prompts;
+		this.promptsFile = promptsFile;
+	}
+
+	// Returns all available suggestions.
+	getSuggestions(query: string): Prompt[] {
+		return this.prompts.filter((prompt) =>
+			prompt.title.toLowerCase().includes(query.toLowerCase())
+		);
+	}
+
+	// Renders each suggestion item.
+	renderSuggestion(prompt: Prompt, el: HTMLElement) {
+		el.createDiv({ text: prompt.title });
+		el.createEl('small', { text: prompt.started ? 'Started' : '' });
+	}
+
+	// Perform action on the selected suggestion.
+	onChooseSuggestion(prompt: Prompt, _evt: MouseEvent | KeyboardEvent) {
+		if (!prompt.started) {
+			this.app.vault.create(prompt.title + '.md', '').then((newFile) => {
+				this.app.vault.process(this.promptsFile, (data) => {
+					return data.replace(prompt.title, `[[${prompt.title}]]`);
+				}).then(() => {
+					this.app.workspace.getLeaf('tab').openFile(newFile);
+				});
+			});
+		} else {
+			const file = this.app.vault.getAbstractFileByPath(prompt.title + '.md');
+			if (file instanceof TFile) {
+				this.app.workspace.getLeaf('tab').openFile(file);
+			}
+		}
+	}
+}
+
+class AddPromptModal extends Modal {
+	promptsFile: TFile;
+
+	constructor(
+		app: App,
+		promptsFile: TFile
+	) {
+		super(app);
+		this.promptsFile = promptsFile;
+
+		this.setTitle('Add a new prompt to the prompts file');
+
+		let prompt = '';
+		new Setting(this.contentEl)
+			.addText((text) => {
+				text.inputEl.style.width = '100%';
+				text.inputEl.addEventListener('keydown', async (e: KeyboardEvent) => {
+					if (e.key === 'Enter' && prompt.trim()) {
+						e.preventDefault();
+						const content = await this.app.vault.read(this.promptsFile);
+						const newLine = (content.endsWith('\n') ? '' : '\n') + prompt.trim() + '\n';
+						await this.app.vault.modify(this.promptsFile, content + newLine);
+						this.close();
+					}
+				});
+				text.onChange((value) => {
+					prompt = value;
+				});
+			});
 	}
 }
